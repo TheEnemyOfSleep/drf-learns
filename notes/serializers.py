e4278373eb160940eba124b9fa3c106bd68d1cef
Amjad Masad <amjad.masad@gmail.com>
2020-08-05 07:48:47 +0000

    Restore merges

--- notes/serializers.py
+++ notes/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from .models import Note
 
-<<<<<<< HEAD
 class NoteSerializer(serializers.ModelSerializer):
     author = serializers.HyperlinkedRelatedField(
         view_name='customuser-detail',
@@ -12,9 +11,3 @@ class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
         fields = ['url', 'id', 'title', 'body', 'author']
-=======
-class NoteSerializer(serializers.HyperlinkedModelSerializer):
-  class Meta:
-    model = Note
-    fields = ['url', 'id', 'title', 'body', 'author']
->>>>>>> 520291b2b15db9c48bf562f464a1c1b8761d8a3b
